fce8fd74da1b4e45d969f6cdd37fd3b0ba676ed4
Valter Lima Mesquita <limavalter@gmail.com>
2017-02-03 10:42:43 -0300

    Added some exception handling

--- CartManagerApplication/Program.cs
+++ CartManagerApplication/Program.cs
@@ -1,6 +1,7 @@
 ﻿using CartManagerApplication.src.level1;
 using CartManagerApplication.src.level2;
 using CartManagerApplication.src.level3;
+using System;
 
 namespace CartManagerApplication
 {
@@ -8,14 +9,51 @@ namespace CartManagerApplication
     {
         static void Main(string[] args)
         {
-            Solution1 sol1 = new Solution1();
-            sol1.run();
-            
-            Solution2 sol2 = new Solution2();
-            sol2.run();
+            try
+            {
+                Solution1 sol1 = new Solution1();
+                sol1.run();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(string.Format("Level 1 ERROR: {0}", ex.Message));
+            }
+            finally
+            {
+                Console.WriteLine("Level 1 solved successfully");
+            }
 
-            Solution3 sol3 = new Solution3();
-            sol3.run();
+            try
+            {
+                Solution2 sol2 = new Solution2();
+                sol2.run();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(string.Format("Level 2 ERROR: {0}", ex.Message));
+            }
+            finally
+            {
+                Console.WriteLine("Level 2 solved successfully");
+            }
+
+            try
+            {
+                Solution3 sol3 = new Solution3();
+                sol3.run();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(string.Format("Level 3 ERROR: {0}", ex.Message));
+            }
+            finally
+            {
+                Console.WriteLine("Level 3 solved successfully");
+            }
+
+
+
+
         }
     }
 }
